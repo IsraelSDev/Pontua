96225be45fc6a53071a444a8b629042d33b814b6
issantos <israelss@primeit.com.br>
2023-07-27 23:14:24 -0300

    lint e prettier

--- src/store/PageContext.tsx
+++ src/store/PageContext.tsx
@@ -1,14 +1,14 @@
 // contextUtils.js
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext } from 'react';
 
 const PageContext = createContext();
 
 export function PageContextProvider({ children }) {
   const [data, setData] = useState({
-    currentPage: "home",
+    currentPage: 'home',
     handleCurrentPage: (playload) => {
       setData((prevState) => ({ ...prevState, currentPage: playload }));
-    }
+    },
   });
 
   return <PageContext.Provider value={data}>{children}</PageContext.Provider>;
